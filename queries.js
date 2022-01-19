3703d6771e78e8bf739b76afb31ba358d880d665
joeyxoay <zhi.koay@mail.mcgill.ca>
2022-01-18 20:29:39 -0700

    getItems API

---
+++ queries.js
@@ -0,0 +1,21 @@
+const Pool = require('pg').Pool
+const pool = new Pool({
+  user: 'postgres',
+  host: 'localhost',
+  database: 'shopify',
+  password: 'Shopify123!',
+  port: 5432,
+})
+
+const getItems = (request, response) => {
+    pool.query('SELECT * FROM public.item WHERE deleted = false ORDER BY id ASC', (error, results) => {
+      if (error) {
+        throw error
+      }
+      response.status(200).json(results.rows)
+    })
+}
+
+module.exports = {
+    getItems
+}
